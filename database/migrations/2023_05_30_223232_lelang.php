c26c46937ca7074e6c5111d1b682f59ca07e4481
ramasakti <ramasakti1337@gmail.com>
2023-06-01 19:39:25 +0700

    Menambahkan Field Slug pada DB Lelang

--- database/migrations/2023_05_30_223232_lelang.php
+++ database/migrations/2023_05_30_223232_lelang.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('lelang', function (Blueprint $table) {
             $table->id('id_lelang');
             $table->string('judul_lelang');
+            $table->text('slug');
             $table->date('mulai_lelang');
             $table->date('selesai_lelang');
         });
